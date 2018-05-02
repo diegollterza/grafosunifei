2f00126de5957d7a77269069affb9aeac65e3a14
diegollterza <diegolaterza@hotmail.com>
2018-05-02 19:25:45 +0000

    Adicionado complemento de um grafo

--- ECOHAS.hs
+++ ECOHAS.hs
@@ -4,6 +4,13 @@ type Grafo = [Vertice]
 grafo :: Grafo
 grafo = [(1,[2,3,4]), (2,[1,4]), (3,[1,4]), (4,[1,2,3])]
 
+listaVertices :: Grafo -> [Int]
+listaVertices [] = []
+listaVertices ((v,l):t) = v:listaVertices(t)
+
+vertices :: [Int]
+vertices = listaVertices(grafo)
+
 grauVertice :: [Int] -> Int
 grauVertice [] = 0
 grauVertice (_:cauda) = 1 + grauVertice(cauda)
@@ -31,5 +38,15 @@ euleriano :: Grafo -> Bool
 euleriano(g)  |arestasImpares(g) == 2 = True  
               |arestasImpares(g) == 0 = True
               |otherwise = False
-              
+
+diferenca :: [Int] -> [Int] -> [Int]
+diferenca [] _ = []
+diferenca (a:l1) l2   |a `elem` l2 = diferenca l1 l2
+                      |otherwise = a:diferenca l1 l2
+
+complemento :: Grafo -> Grafo
+
+complemento [] = []
+complemento ((v,l):t) = (v,diferenca vertices (v:l)):complemento(t)
+
 main = print grafo
